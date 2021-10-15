f535e5353a8fab2ce1bcb3c33db90f682e5b6f92
dev-from-cbd <veryfatwombat@gmail.com>
2021-10-16 10:21:55 +1100

    comments

--- serv/src/track/track.controller.ts
+++ serv/src/track/track.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from "@nestjs/common";
+/*
+import {Body, Controller, Delete, Get, Param, Post, Query, UploadedFiles, UseInterceptors} from "@nestjs/common";
+import {TrackService} from "./track.service";
+import {CreateTrackDto} from "./dto/create-track.dto";
+import {ObjectId} from "mongoose";
+import {CreateCommentDto} from "./dto/create-comment.dto";
+import {FileFieldsInterceptor} from "@nestjs/platform-express";
+*/
 
 @Controller('/tracks')
 export class TrackController {
