14f2860044ecd270689e36aca031bfe1cb5fa756
dev-from-cbd <veryfatwombat@gmail.com>
2021-10-20 08:49:23 +1100

    ts

--- serv/src/app.module.ts
+++ serv/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+//import { AppController } from "./app.controller";
+//import { AppService } from "./app.service";
 import { TrackModule } from "./track/track.module";
 //import {MongooseModule} from "@nestjs/mongoose";
 //import {FileModule} from "./file/file.module";
@@ -6,6 +8,9 @@ import { TrackModule } from "./track/track.module";
 //import {ServeStaticModule} from "@nestjs/serve-static";
 
 @Module({
+  //controllers: [AppController],
+  //providers: [AppService],
+
   imports: [
     //        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
     //  MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.q4ykt.mongodb.net/music-platform?retryWrites=true&w=majority'),
